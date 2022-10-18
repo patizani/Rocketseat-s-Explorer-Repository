306a5ee071a824b04727b165e6b6ce6323cebcb6
patizani <pzanirati@gmail.com>
2022-10-18 16:54:38 -0300

    created folder api

---
+++ stage-11/Challenge-1/api/src/routes/index.js
@@ -0,0 +1,19 @@
+//Arquivo que identifica a rota que o usuário está pedindo
+const { Router } = require("express");
+
+const usersRouter = require("./users.routes")
+const notesRouter = require("./notes.routes")
+const tagsRouter = require("./tags.routes")
+const sessionsRouter = require("./sessions.routes")
+const movieNotesRoutes = require("./movienotes.routes")
+
+const routes = Router();
+
+routes.use("/users", usersRouter);
+routes.use("/sessions", sessionsRouter);
+routes.use("/notes", notesRouter);
+routes.use("/tags", tagsRouter);
+routes.use("/movies", movieNotesRoutes);
+
+
+module.exports = routes;
