d6745a97f99505d404217a487e030e9f45453bf4
Patrícia Zanirati <72117473+patizani@users.noreply.github.com>
2022-10-25 10:02:21 -0300

    Delete stage-11/Challenge-1/RocketMovies directory

--- stage-11/Challenge-1/RocketMovies/src/components/ButtonText/styles.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const Container = styled.button`
-    background: none;
-    color: ${({theme, isActive}) => isActive ? theme.COLORS.PINK : theme.COLORS.GRAY_100} ;
-    border: none;
-    font-size: 16px;
-`;
