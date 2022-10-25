80aaf708d9297575507b15d2461a4f72cc43fc80
Patrícia Zanirati <72117473+patizani@users.noreply.github.com>
2022-10-25 10:04:03 -0300

    Delete stage-11/Challenge-1/api directory

--- stage-11/Challenge-1/api/src/database/knex/index.js
+++
@@ -1,7 +0,0 @@
-//Configuração do knex com o banco de dados.
-const config = require('../../../knexfile');
-const knex = require('knex');
-
-const connection = knex(config.development);
-
-module.exports = connection;
